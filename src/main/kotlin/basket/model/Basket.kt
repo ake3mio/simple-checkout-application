c6478b5df6e4423ca6353674d7b0aefaffe59273
akeemfaulkner <contact@ake3m.com>
2021-07-18 17:16:18 +0100

    Implements Basket

--- src/main/kotlin/basket/model/Basket.kt
+++ src/main/kotlin/basket/model/Basket.kt
@@ -1,5 +1,6 @@
 package basket.model
 
+import product.Inventory
 import java.math.BigDecimal
 
 data class Basket(val items: Map<Char, BasketItem> = emptyMap()) {
@@ -9,10 +10,61 @@ data class Basket(val items: Map<Char, BasketItem> = emptyMap()) {
     }
 
     fun addItem(itemSku: Char): Basket {
-        return copy()
+        if (items.containsKey(itemSku)) {
+            return addQuantity(itemSku)
+        }
+        return addNew(itemSku)
     }
 
     fun getBasketTotal(): BigDecimal {
-        return BigDecimal.ZERO
+        if (items.isEmpty()) {
+            return BigDecimal.ZERO
+        }
+
+        return items
+            .values
+            .map { it.product.unitPrice.multiply(BigDecimal(it.quantity)) }
+            .reduce { prev, curr -> prev.add(curr) }
+    }
+
+    private fun addNew(itemSku: Char): Basket {
+        return Inventory
+            .findProductBySku(itemSku)
+            ?.let { product ->
+                val items = items
+                    .map { it.key to copyItem(it.value) }
+                    .toMap() +
+                    mapOf(itemSku to BasketItem(product, 1))
+
+                copy(items = items)
+            } ?: copy()
+    }
+
+    private fun addQuantity(itemSku: Char): Basket {
+        if (items.containsKey(itemSku)) {
+            val items = items.map {
+                it.key to if (itemSku == it.value.product.itemSku) {
+                    updateItemQty(it.value)
+                } else {
+                    copyItem(it.value)
+                }
+            }.toMap()
+            return copy(items = items)
+        }
+        return copy()
+    }
+
+    private fun copyItem(basketItem: BasketItem): BasketItem {
+        return basketItem.copy(
+            product = basketItem.product.copy(),
+            quantity = basketItem.quantity
+        )
+    }
+
+    private fun updateItemQty(basketItem: BasketItem): BasketItem {
+        return basketItem.copy(
+            product = basketItem.product.copy(),
+            quantity = basketItem.quantity + 1
+        )
     }
 }
