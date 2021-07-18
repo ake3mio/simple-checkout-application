7a966ea6c2e0e01f11121580f3c7132e4a4c192c
akeemfaulkner <contact@ake3m.com>
2021-07-18 17:17:48 +0100

    Implements SpecialPriceCondition

--- src/main/kotlin/specialprice/model/SpecialPriceCondition.kt
+++ src/main/kotlin/specialprice/model/SpecialPriceCondition.kt
@@ -1,12 +1,34 @@
 package specialprice.model
 
 import basket.model.Basket
+import basket.model.BasketItem
 
 data class SpecialPriceCondition(
     private val quantity: Int,
     private val itemSku: Char
 ) {
     fun matches(basket: Basket): Pair<Char, Int>? {
-        return null
+        return basket.items.values
+            .find { it.product.itemSku == itemSku }
+            ?.let(this::getMatches)
+    }
+
+    private fun getMatches(basketItem: BasketItem): Pair<Char, Int> {
+        return if (basketItem.quantity > 0 && quantity > 0) {
+            Pair(
+                basketItem.product.itemSku,
+                Math.floorDiv(basketItem.quantity, quantity)
+            )
+        } else if (basketItem.quantity == quantity) {
+            Pair(
+                basketItem.product.itemSku,
+                1
+            )
+        } else {
+            Pair(
+                basketItem.product.itemSku,
+                0
+            )
+        }
     }
 }
